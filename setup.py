e2d169fb9e27b42d82a875e08efa5ef3757f19de
Antonin Lacombe <antonin.lacombe@gmail.com>
2013-05-11 16:45:05 +0200

    [I] add exercise description on readme and fix setup.py

--- setup.py
+++ setup.py
@@ -5,33 +5,30 @@
 
 # word_checker
 # Version
-from os.path import abspath, dirname, join
-
-try:
-    __version__=file(join(root, "version")).readline().rstrip().rstrip("\n")
-except Exception, e:
-    print "Warning, cannot read version file (%s)" % e
-    __version__="0.0.1"
-    
-# system
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-from os.path import join, dirname
+import sys
+from setuptools import setup
+from word_checker import __version__
 
 
 setup(
     name="word_checker",
     version=__version__,
     description='a simple python word checker',
+    license='LGPLv3',
     author='Antonin Lacombe',
     author_email='antonin.lacombe@gmail.com',
-   package_dir={"word_checker" : "src/word_checker"},
+    package_dir={"word_checker" : "src/word_checker"},
     url='https://github.com/iXioN/word_checker',
     long_description=open('README.md').read(),
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'Programming Language :: Python',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Natural Language :: English',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
       ],
 )
---
+++ src/word_checker/__init__.py
@@ -0,0 +1,8 @@
+# 
+#  __init__.py
+#  word_checker
+#  
+#  Created by Antonin Lacombe on 2013-05-11.
+#  Copyright 2013 Antonin Lacombe. All rights reserved.
+# 
+__version__ = '0.1-dev'
